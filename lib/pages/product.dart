a767a1302cf4ae32aa2722128bac21a2b6ca8f0e
Shawn Loh <shawnlohb@gmail.com>
2019-05-22 21:16:41 +0800

    removed WillPopScope to Just Scaffold so that it can have back gesture to return back to previous screen.

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -6,38 +6,85 @@ class ProductPage extends StatelessWidget {
 
   ProductPage(this.title, this.imageUrl);
 
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () {
-        Navigator.of(context).pop(false);
-        return Future.value(false);
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              child: Text('On the product page'),
-              padding: EdgeInsets.all(10.0),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('DELETE'),
+  _showWarningDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('This action is irreversible'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancel'),
                 onPressed: () {
-                  Navigator.of(context).pop(true);
+                  Navigator.pop(context);
                 },
               ),
-            )
-          ],
-        ),
+              FlatButton(
+                child: Text('Delete'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context, true);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(imageUrl),
+          Container(
+            child: Text('On the product page'),
+            padding: EdgeInsets.all(10.0),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('DELETE'),
+              onPressed: () => _showWarningDialog(context),
+            ),
+          )
+        ],
       ),
     );
+//    return WillPopScope(
+//      onWillPop: () {
+//        Navigator.of(context).pop(false);
+//        return Future.value(false);
+//      },
+//      child: Scaffold(
+//        appBar: AppBar(
+//          title: Text(title),
+//        ),
+//        body: Column(
+//          crossAxisAlignment: CrossAxisAlignment.center,
+//          children: <Widget>[
+//            Image.asset(imageUrl),
+//            Container(
+//              child: Text('On the product page'),
+//              padding: EdgeInsets.all(10.0),
+//            ),
+//            Container(
+//              padding: EdgeInsets.all(10.0),
+//              child: RaisedButton(
+//                color: Theme.of(context).accentColor,
+//                child: Text('DELETE'),
+//                onPressed: () => _showWarningDialog(context),
+//              ),
+//            )
+//          ],
+//        ),
+//      ),
+//    );
   }
 }
