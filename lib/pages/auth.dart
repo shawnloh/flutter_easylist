6c3d454ca7e966ddb0ef2a673cea95f1e7e327a5
Shawn Loh <shawnlohb@gmail.com>
2019-05-22 21:15:55 +0800

    Change auth to stateful widget

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,24 +1,64 @@
 import 'package:flutter/material.dart';
-import 'products.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  _AuthPageState createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _emailValue = '';
+  String _passwordValue = '';
+  bool _tos = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Login'),
-          onPressed: () {
-            final route = MaterialPageRoute(
-              builder: (BuildContext context) {
-                return ProductsPage();
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (val) {
+                setState(() {
+                  _emailValue = val;
+                });
               },
-            );
-            Navigator.pushReplacement(context, route);
-          },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              onChanged: (val) {
+                setState(() {
+                  _passwordValue = val;
+                });
+              },
+            ),
+            SwitchListTile(
+              title: Text('Accept Terms'),
+              onChanged: (val) {
+                setState(() {
+                  _tos = val;
+                });
+              },
+              value: _tos,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
+              child: Text('Login'),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/products');
+              },
+            ),
+          ],
         ),
       ),
     );
